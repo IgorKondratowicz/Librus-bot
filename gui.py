c2c3c3b1691e24b0608279174adb92d2fe326a61
IgorKondratowicz <igorkondratowicz2003@gmail.com>
2022-02-09 12:35:43 +0100

    code cleaned

--- gui.py
+++ gui.py
@@ -1,42 +1,45 @@
+"""
+    Simple GUI to Librus bot
+"""
+
 import tkinter as tk
-from tkinter import BOTTOM, LEFT, font
+from tkinter import BOTTOM
 
 class Application(tk.Tk):
+    """
+        Librus GUI
+    """
     def __init__(self):
         super().__init__()
+        self.login, self.password  = "", ""
         self.geometry("400x300")
         self.title("Librus Bot")
         self.resizable(0, 0)
-        self.createElements()
 
-    def getEntryValues(self):
+    def get_entry_values(self):
+        """ Getting values from input """
+        
         self.login = self.login_entry.get()
         self.password = self.password_entry.get()
         self.destroy()
-    
 
-    def createElements(self):
-        #labels
+    def create_elements(self):
+        """ Creating and placing elements """
+
         self.main_label = tk.Label(text = "LOGOWANIE", font = ("Verdana", 15), fg = "red")
         self.main_label.pack(pady = 20)
-
         self.login_label = tk.Label(text = "Login: ", font = ("Verdana", 14))
         self.login_label.place(x = 10, y = 100)
-
         self.password_label = tk.Label(text = "Password: ", font = ("Verdana", 14))
         self.password_label.place(x = 10, y = 170)
-
-        #inputs
         self.login_entry = tk.Entry()
         self.login_entry.place(x = 110, y = 105)
-
         self.password_entry = tk.Entry()
         self.password_entry.place(x = 140, y = 175)
-
-        #submit button
-        self.button = tk.Button(text = "Zaloguj", command = self.getEntryValues).pack(side=BOTTOM, pady = 20)
+        self.button = tk.Button(text = "Zaloguj", command = self.get_entry_values)
+        self.button.pack(side=BOTTOM, pady = 20)
 
 if __name__ == "__main__":
     app = Application()
+    app.create_elements()
     app.mainloop()
-    
--- librus_bot.py
+++ librus_bot.py
@@ -1,59 +1,68 @@
+"""
+    Written by Igor Kondratowicz, 09.02.2022
+"""
+
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from gui import Application
-from time import sleep
-
 
 class Librus:
+    """
+        Logging into Librus using tkinter module and selenium webdriver
+    """
     def __init__(self, login, password):
         self.base_url = "https://portal.librus.pl/rodzina/home"
         options = Options()
         options.add_experimental_option("detach", True)
-        
         self.login = login
         self.password = password
-
         self.driver = webdriver.Chrome(options=options)
         self.driver.maximize_window()
         self.driver.implicitly_wait(0.5)
         self.driver.get(self.base_url)
-        sleep(2)
+        self.main()
 
-        self.synergia(self.login, self.password)
+    def __repr__(self):
+        return "Librus bot"
 
-        
-        
-    def synergia(self, login, password):
+    def redirect(self) -> None:
+        """
+        Redirecting to Librus login page
+        """
+
         box = self.driver.find_element(By.CLASS_NAME, "btn-synergia-top")
         box.click()
         sleep(2)
-
         menu_box = self.driver.find_elements(By.CLASS_NAME, "dropdown-item--synergia")
         menu_box[1].click()
         sleep(2)
-        
         self.driver.switch_to.frame("caLoginIframe")
 
+
+    def log_in(self) -> None:
+        """
+        Logging into Synergia
+        """
         login_box = self.driver.find_element(By.ID, "Login")
         pass_box = self.driver.find_element(By.ID, "Pass")
-
-        login_box.send_keys(login)
+        login_box.send_keys(self.login)
         sleep(1)
-        pass_box.send_keys(password)
+        pass_box.send_keys(self.password)
         sleep(1)
         log_button = self.driver.find_element(By.ID, 'LoginBtn')
-        log_button.click() 
+        log_button.click()
 
-        
-        
+    def main(self) -> None:
+        """
+        Main function
+        """
+        self.redirect()
+        self.log_in()
+
 if __name__ == "__main__":
     gui = Application()
     gui.mainloop()
-    try: 
-        if gui.login != "" and gui.password != "":
-            Librus(gui.login, gui.password)
-    except:
-        pass
-    
-    
+    if gui.login != "" and gui.password != "":
+        Librus(gui.login, gui.password)
